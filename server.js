3b53ba207fe0f7d45ea642ed1822f2c6d63500ac
Ismael OUEDRAOGO <puissancisma@gmail.com>
2020-02-11 20:29:02 +0000

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Syslog = require('simple-syslog-server') ;
- 
+var log= require('noogger');
 // Create our syslog server with the given transport
 const socktype = 'TCP' ; // or 'TCP' or 'TLS'
 const address = '' ; // Any
@@ -12,7 +12,7 @@ var clients = [] ;
 var count = 0 ;
  
 server.on('msg', data => {
-    console.log('message received (%i) from %s:%i\n%o\n', ++count, data.address, data.port, data) ;
+    log.notice('message received (%i) from %s:%i\n%o\n', ++count, data.address, data.port, data) ;
     /*
     message received (1) from ::ffff:192.168.1.13:59666
     {
@@ -32,17 +32,17 @@ server.on('msg', data => {
     */
 })
 .on('invalid', err => {
-    console.warn('Invalid message format received: %o\n', err) ;
+    log.warning('Invalid message format received: %o\n', err) ;
 })
 .on('error', err => {
-    console.warn('Client disconnected abruptly: %o\n', err) ;
+    log.warning('Client disconnected abruptly: %o\n', err) ;
 })
 .on('connection', s => {
     let addr = s.address().address ;
-    console.log(`Client connected: ${addr}\n`) ;
+   log.debug(`Client connected: ${addr}\n`) ;
     clients.push(s) ;
     s.on('end', () => {
-        console.log(`Client disconnected: ${addr}\n`) ;
+       log.debug(`Client disconnected: ${addr}\n`) ;
         let i = clients.indexOf(s) ;
         if(i !== -1)
             clients.splice(i, 1) ;
@@ -51,20 +51,20 @@ server.on('msg', data => {
 .listen({host: address, port: port})
 .then(() => {
     listening = true ;
-    console.log(`Now listening on: ${address}:${port}`) ;
+   log.debug(`Now listening on: ${address}:${port}`) ;
 })
 .catch(err => {
     if ((err.code == 'EACCES') && (port < 1024)) {
-        console.error('Cannot listen on ports below 1024 without root permissions. Select a higher port number: %o', err) ;
+        log.error('Cannot listen on ports below 1024 without root permissions. Select a higher port number: %o', err) ;
     }
     else { // Some other error so attempt to close server socket
-        console.error(`Error listening to ${address}:${port} - %o`, err) ;
+        log.error(`Error listening to ${address}:${port} - %o`, err) ;
         try {
             if(listening)
                 server.close() ;
         }
         catch (err) {
-            console.warn(`Error trying to close server socket ${address}:${port} - %o`, err) ;
+            log.warning(`Error trying to close server socket ${address}:${port} - %o`, err) ;
         }
     }
 }) ;
